0e8e17c314e53ac3b48e64655b129de1c427893d
luck <hsdsmjay@gmail.com>
2017-11-01 16:41:22 +0800

    add fetchall

--- fetchall.go
+++ fetchall.go
@@ -1,4 +1,4 @@
-//
+// 1. use of gorountine and channel
 package main
 
 import (
@@ -12,4 +12,30 @@ import (
 
 func main() {
 	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) //launch a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) //receive from channel, pass value of channel: <-ch
+	}
+	fmt.Printf("%.2fs elpsed\n", time.Since(start).Seconds())
+}
+
+// http get and return info to channel
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	rsp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, rsp.Body)
+	rsp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprint("while reading %s: %v", url, err) //pass expression to channel: ch<-[expression]
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
